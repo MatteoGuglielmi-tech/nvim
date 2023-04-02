9cab4aeffe1d917c4ee338075f0c8667f9262a21
Matteo-Tech <matteo.guglielmi.ma.gu@gmail.com>
2023-04-02 16:22:20 +0200

    :adhesive_bandage: Fix: removed bashls from default configured servers table. Gives problems

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,95 +3,95 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then
-	return
+    return
 end
 
 -- servers configred with default features
-local servers = { "bashls", "marksman" }
+local servers = { "marksman" }
 
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
 end
 
-lspconfig.lua_ls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+lspconfig.lua_ls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
 
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				},
-				maxPreload = 100000,
-				preloadFileSize = 10000,
-			},
-		},
-	},
-})
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                    [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+        },
+    },
+}
 
-lspconfig.pyright.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	cmd = { "pyright-langserver", "--stdio" },
-	settings = {
-		python = {
-			analysis = {
-				autoSearchPaths = true,
-				useLibraryCodeForTypes = true,
-				typeCheckingMode = "basic", -- options are "off", "basic", "strict"
-				diagnosticMode = "workspace",
-				inlayHints = {
-					variableTypes = true,
-					functionReturnTypes = true,
-				},
-			},
-		},
-	},
-	single_file_support = true,
-})
+lspconfig.pyright.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "pyright-langserver", "--stdio" },
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = "basic", -- options are "off", "basic", "strict"
+                diagnosticMode = "workspace",
+                inlayHints = {
+                    variableTypes = true,
+                    functionReturnTypes = true,
+                },
+            },
+        },
+    },
+    single_file_support = true,
+}
 
-lspconfig.texlab.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	cmd = { "texlab" },
-	settings = {
-		texlab = {
-			build = {
-				args = {
-					"-pdf",
-					"-interaction=nonstopmode",
-					"-synctex=1",
-					"%f",
-				},
-				executable = "latexmk",
-				onSave = true,
-			},
-			chktex = {
-				onEdit = true,
-				onOpenAndSave = false,
-			},
-			forwardSearch = {
-				executable = "zathura",
-				args = { "--synctex-forward", "%1:1:%f", "%p" },
-			},
-			lint = {
-				onChange = true,
-			},
-			latexFormatter = "latexindent",
-			latexindent = {
-				modifyLineBreaks = true,
-			},
-		},
-	},
+lspconfig.texlab.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "texlab" },
+    settings = {
+        texlab = {
+            build = {
+                args = {
+                    "-pdf",
+                    "-interaction=nonstopmode",
+                    "-synctex=1",
+                    "%f",
+                },
+                executable = "latexmk",
+                onSave = true,
+            },
+            chktex = {
+                onEdit = true,
+                onOpenAndSave = false,
+            },
+            forwardSearch = {
+                executable = "zathura",
+                args = { "--synctex-forward", "%1:1:%f", "%p" },
+            },
+            lint = {
+                onChange = true,
+            },
+            latexFormatter = "latexindent",
+            latexindent = {
+                modifyLineBreaks = true,
+            },
+        },
+    },
 
-	filetypes = { "tex", "bib", "plaintex" },
-	single_file_support = true,
-})
+    filetypes = { "tex", "bib", "plaintex" },
+    single_file_support = true,
+}
