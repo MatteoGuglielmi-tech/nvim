6885a1fbccc4129ae246afd30438aec23307eb3c
Matteo-Tech <matteo.guglielmi.ma.gu@gmail.com>
2023-04-04 21:25:34 +0200

    :wrench: update: cleaning
    lspconfig: removed useless code actions. null-ls: autoimports for pyright. overrides: removed comment

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -52,6 +52,8 @@ lspconfig.pyright.setup {
                     variableTypes = true,
                     functionReturnTypes = true,
                 },
+                -- set pyright to remove unused imports when saving
+                autoImportCompletion = true,
             },
         },
     },
--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -1,51 +1,49 @@
 local M = {}
 
 M.treesitter = {
-	ensure_installed = {
-		"vim",
-		"lua",
-		"python",
-		"latex",
-		"bash",
-		"c",
-		"cpp",
-		"markdown",
-		"markdown_inline",
-	},
-	indent = {
-		enable = true,
-		disable = {},
-	},
+    ensure_installed = {
+        "vim",
+        "lua",
+        "python",
+        "latex",
+        "bash",
+        "c",
+        "cpp",
+        "markdown",
+        "markdown_inline",
+    },
+    indent = {
+        enable = true,
+        disable = {},
+    },
 }
 
 M.mason = {
-	ensure_installed = {
-		"lua-language-server",
-		"stylua",
-		"lua-language-server",
-		"clangd",
-		"pyright",
-		"bashls",
-		"texlab",
-		"marksman",
-	},
+    ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "lua-language-server",
+        "clangd",
+        "pyright",
+        "bashls",
+        "texlab",
+        "marksman",
+    },
 }
 
--- Add null-ls
-
 M.nvimtree = {
-	git = {
-		enable = true,
-	},
+    git = {
+        enable = true,
+    },
 
-	renderer = {
-		highlight_git = true,
-		icons = {
-			show = {
-				git = true,
-			},
-		},
-	},
+    renderer = {
+        highlight_git = true,
+        icons = {
+            show = {
+                git = true,
+            },
+        },
+    },
 }
 
 return M
