2e96d1bc836b4933a03e34c8357b7b06834ed621
Matteo-Tech <matteo.guglielmi.ma.gu@gmail.com>
2023-04-13 22:21:07 +0200

    :wrench: update: fancy greeter

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,17 +1,19 @@
 local M = {}
 
-local highlights = require("custom.highlights")
+local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light"},
-
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+    theme = "onedark",
+    theme_toggle = { "onedark", "one_light" },
+    nvdash = {
+        load_on_startup = true,
+    },
+    hl_override = highlights.override,
+    hl_add = highlights.add,
 }
 
 M.plugins = "custom.plugins"
 
-M.mappings = require("custom.mappings")
+M.mappings = require "custom.mappings"
 
 return M
